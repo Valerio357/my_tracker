e779d3f5f9171264b47419aaae74a4212899612c
Valerio357 <62748844+Valerio357@users.noreply.github.com>
2023-11-26 19:34:28 +0100

    new update over display and data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import math 
+import pandas as pd
 
 # Define the nutritional information for each food item
 tuna_calories = 142
@@ -12,44 +12,45 @@ bresaola_calories = 150
 # Define the nutritional information for each food item
 tuna_protein = 25
 egg_protein = 13
-
 steak_protein = 21
 chicken_protein = 31
 bresaola_protein = 35
 
-
 # Create a title and subtitle for the app
-st.title("Calorie and Protein Tracker")
-st.subheader("Calculate your calorie and protein intake from tuna, eggs, and steak")
+st.title("🥩 Protein Tracker")
+st.subheader("Calculate your calories and protein intake from differnt kind of proteins")  
 
 # Create input fields for calories and protein per kg
-protein_per_kg = st.number_input("Enter the desired protein intake per kg (in grams):")
-weight_in_kg = st.number_input("Enter your weight (in kgs):")
+protein_per_kg = st.number_input("Enter the desired protein intake per kg (g):", step=0.1)
+weight_in_kg = st.slider("Enter your weight (in kgs):", step=1, max_value=120)
 
 # Create input fields for weight and protein per kg
-tuna_weight = st.number_input("Enter the weight of tuna (in grams):")
-egg_weight = st.number_input("Enter the weight of eggs (in grams):")
-steak_weight = st.number_input("Enter the weight of steak (in grams):")
-chicken_weight = st.number_input("Enter the weight of chicken (in grams):")
-bresaola_weight = st.number_input("Enter the weight of breasola (in grams):")
-
-
+tuna_weight = st.number_input("Enter the weight of tuna (g):", step=100)
+egg_weight = st.number_input("Enter the weight of eggs (g):", step=100)
+steak_weight = st.number_input("Enter the weight of steak (g):", step=100)
+chicken_weight = st.number_input("Enter the weight of chicken (g):", step=100)
+bresaola_weight = st.number_input("Enter the weight of breasola (g):", step=100)
+    
 # Calculate the total calories and protein intake
 total_calories = tuna_weight * tuna_calories / 100 + egg_weight * egg_calories / 100 + steak_weight * steak_calories / 100 + chicken_weight * chicken_calories / 100 + bresaola_weight * bresaola_calories / 100
 total_protein = tuna_weight * tuna_protein / 100 + egg_weight * egg_protein / 100 + steak_weight * steak_protein / 100 + chicken_weight * chicken_protein / 100 + bresaola_weight * bresaola_protein / 100
 
-# Display the total calories and protein intake
-st.subheader("Total Calories:")
-st.write(total_calories)
-st.subheader("Total Protein:")
-st.write(total_protein)
 
-# Calculate the percentage of calories from protein
+
 try:
-    protein_percentage = math.floor(total_protein / (weight_in_kg*protein_per_kg))
-except ZeroDivisionError:
+    protein_percentage = total_protein / (weight_in_kg*protein_per_kg)
+except:
     protein_percentage = 0
 
+
 # Display the percentage of calories from protein
-st.subheader("Percentage of Calories from Protein:")
-st.write(protein_percentage)
+formatted_percentage = '{:.2%}'.format(protein_percentage)
+if  protein_percentage <= 0.5:
+    st.title(f''':red[You covered {formatted_percentage} of your protein intake and consumed {total_calories}]''')
+elif 0.5 <protein_percentage <= 0.7:
+    st.title(f''':orange[You covered {formatted_percentage} of your protein intake and consumed {total_calories}]''')
+else:
+    st.title(f''':green[You covered {formatted_percentage} of your protein intake and consumed {total_calories}]''')
+
+
+df = pd.DataFrame([protein_percentage])
